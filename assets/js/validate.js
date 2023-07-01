e95d831c25801633bdf36b60123c68af1437f205
Claudio <claudiom.pst@gmail.com>
2023-06-30 22:43:46 -0300

    adding new elements such as changing details

--- assets/js/validate.js
+++ assets/js/validate.js
@@ -1,1 +1,13 @@
+window.onscroll = function() {
+    scrollFunction();
+}
+  
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      document.getElementById("scrollToTop").style.display = "block";
+    } else {
+      document.getElementById("scrollToTop").style.display = "none";
+    }
+}
+  
 //Seu JavaScript de validação aqui
