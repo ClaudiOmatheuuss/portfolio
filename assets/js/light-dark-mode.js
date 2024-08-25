dcc79146b952585b53229b604c0ae6ef931dee20
Claud10Azeved0 <claudiom.pst@gmail.com>
2024-08-25 18:39:14 -0300

    implementing dark mode and scroll animations

---
+++ assets/js/light-dark-mode.js
@@ -0,0 +1,40 @@
+// script para light e dark mode
+
+document.addEventListener('DOMContentLoaded', () => {
+    const currentTheme = localStorage.getItem('theme');
+    const themeToggleButton = document.querySelector('.theme-toggle');
+
+    document.body.setAttribute('data-theme', currentTheme);
+
+    themeToggleButton.addEventListener('click', () => {
+        if (document.body.getAttribute('data-theme') === 'dark') {
+            document.body.setAttribute('data-theme', 'light');
+            localStorage.setItem('theme', 'light');
+            updateImageSource()
+        } else {
+            document.body.setAttribute('data-theme', 'dark');
+            localStorage.setItem('theme', 'dark');
+            updateImageSource()
+        }
+    });
+
+    // elimina texto no botão ao ser clicado
+    const toggle_btn_textElement = themeToggleButton.querySelector('span');
+
+    themeToggleButton.addEventListener("click", () => {
+        toggle_btn_textElement.textContent = '';
+    })
+});
+
+function updateImageSource() {
+    const currentTheme = localStorage.getItem('theme');
+    const imgElement = document.querySelector('.theme-indicator');
+
+    if (currentTheme === 'light') {
+        imgElement.src = './assets/img/icons/moon.png';
+    } else {
+        imgElement.src = './assets/img/icons/sun.png';
+    }
+}
+
+window.onload = updateImageSource();
--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -1,12 +1,28 @@
 // Script de funcionamento do botão que ao ser clicado retorna ao inicio do scroll
-window.onscroll = function() {
-    scrollFunction();
+window.onscroll = function () {
+  scrollFunction();
 }
-  
+
 function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-      document.getElementById("scrollToTop").style.display = "block";
-    } else {
-      document.getElementById("scrollToTop").style.display = "none";
-    }
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("scrollToTop").style.display = "block";
+  } else {
+    document.getElementById("scrollToTop").style.display = "none";
+  }
 }
+
+// Script para animação no scroll
+
+const myObserver = new IntersectionObserver((entries) => {
+  entries.forEach( (entry) => {
+    if(entry.isIntersecting) {
+      entry.target.classList.add('show');
+    } else {
+      entry.target.classList.remove('show');
+    }
+  })  
+})
+
+const elements = document.querySelectorAll(".hidden");
+
+elements.forEach( (element) => { myObserver.observe(element) })
